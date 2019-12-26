945f54cf511aee0f276f2d2e1f77a97e653be1f8
whitfield <10842089@qq.com>
2019-12-26 09:22:06 +0800

    部署mysql到服务器

--- ECNU-Testing-MeetHereSpring/Meethere/src/main/java/ecnu/testing/meethere/MeetHereApplication.java
+++ ECNU-Testing-MeetHereSpring/Meethere/src/main/java/ecnu/testing/meethere/MeetHereApplication.java
@@ -2,13 +2,20 @@ package ecnu.testing.meethere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "ecnu.testing.meethere.mapper")
-public class MeetHereApplication {
+public class MeetHereApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MeetHereApplication.class, args);
     }
 
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
